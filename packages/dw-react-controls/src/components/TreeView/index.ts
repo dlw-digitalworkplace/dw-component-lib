1b511e4ba3228bc9fa9604b19ff57ed38d6f5eb9
Nick Sevens <nick.sevens@delaware.pro>
2021-07-15 13:11:13 +0200

    Fix default exports

--- packages/dw-react-controls/src/components/TreeView/index.ts
+++ packages/dw-react-controls/src/components/TreeView/index.ts
@@ -1,4 +1,5 @@
 export * from "./TreeView";
 export * from "./TreeView.base";
 export * from "./TreeView.context";
+export { default as TreeViewContext } from "./TreeView.context";
 export * from "./TreeView.types";
--- packages/dw-refresh-cache/src/index.ts
+++ packages/dw-refresh-cache/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./MemoryStorage";
+export { default as MemoryStorage } from "./MemoryStorage";
 export * from "./models";
 export * from "./RefreshCache";
