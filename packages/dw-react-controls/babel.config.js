9cc2158baaee28ea01610d2da83ca7be5ff0006f
Nick Sevens <nick.sevens@delaware.pro>
2023-01-05 14:59:42 +0100

    wip: try to make tests work

--- packages/dw-react-controls/babel.config.js
+++ packages/dw-react-controls/babel.config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	presets: [
 		["@babel/preset-env", { targets: { node: "current", esmodules: true } }],
-		["@babel/preset-react", { runtime: "automatic" }],
 		["@babel/preset-typescript", { allowNamespaces: true }]
 	]
 };
